144171847aeb6054ad4f18e11e001cfb418d23d3
uselesspart <uselesspartinfo@gmail.com>
2026-03-31 10:13:58 +0300

    Date parsing

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.12
 WORKDIR /app
 COPY ./main.py /app
 COPY ./requirements.txt /app
+COPY ./contract_template.docx /app
 
 RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from fastapi import FastAPI, HTTPException, status
+from docx import Document
+from docx.shared import Pt
+from datetime import datetime
+
+ru_months = {
+    "January" : "января",
+    "February" : "февраля",
+    "March" : "марта",
+    "April" : "апреля",
+    "May" : "мая",
+    "June" : "июня",
+    "July" : "июля",
+    "August" : "августа",
+    "September" : "сентября",
+    "October" : "октября",
+    "November" : "ноября",
+    "December" : "декабря"
+}
 
 app = FastAPI()
 items = []
@@ -6,13 +24,44 @@ items_id = 1
 
 @app.get('/')
 def root():
-    return {'message': 'Hello World'}
+    return {'message': 'Hello World!'}
 
 @app.post('/generate_docs/')
 def generate_docs(
-    tour_agent_inn,
-    tour_agent_ogrn,
-    tour_agent_org_name,
-    tour_agent_price,
-    tour_agent_date):
-    pass
+        customer_inn:str,
+        customer_ogrn:str,
+        customer_org_name:str,
+        tour_price,
+        tour_date,
+        contract_date:datetime,
+        tour_cost):
+    template = Document('contract_template.docx')
+
+    #Date parsing
+    if contract_date:
+        contract_day = contract_date.day
+        contract_month = contract_date.strftime("%B")
+        contract_month_ru = ru_months[contract_month]
+        contract_year = contract_date.year
+        for p in template.paragraphs:
+            if 'contract_date' in p.text:
+                p.text = p.text.replace('contract_date', f'«{contract_day}» {contract_month_ru} {contract_year}г.')
+                for run in p.runs:
+                    run.font.name = "Times New Roman"
+                    run.font.size = Pt(12)
+    
+    for table in template.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for p in cell.paragraphs:
+                    for run in p.runs:
+                            if 'customer_org_name' in run.text:
+                                run.text = run.text.replace('customer_org_name', customer_org_name)
+                            if 'customer_ogrn' in run.text:
+                                run.text = run.text.replace('customer_ogrn', f'ОГРН {customer_ogrn}')
+                            if 'customer_inn' in run.text:
+                                run.text = run.text.replace('customer_inn', f'ИНН {customer_inn}')
+                            run.font.name = "Times New Roman"
+                            run.font.size = Pt(12)
+    template.save('result.docx')
+    return("0")
